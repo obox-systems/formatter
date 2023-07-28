55b46a30fd31f55e1980970ec113b20d508d3694
f01dab1e <andriy.n@obox.systems>
2023-07-28 20:16:03 +0000

    add wca

--- src/bin/formatter.rs
+++ src/bin/formatter.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use itertools::Itertools;
+use wca::stdx as wca_cpp;
+
+fn main() {
+    let cli = wca_cpp::cli(()).build();
+
+    let args = std::env::args().skip(1).join(" ");
+    if let Err(message) = cli.perform(args) {
+        eprintln!("{message}");
+    }
+}
