70a45d03fbecf83bff17cd69e2389e9f371e68b7
f01dab1e <andriy.n@obox.systems>
2023-08-15 09:15:54 +0000

    kill old code

--- src/ir.rs
+++
@@ -1,7 +0,0 @@
-pub(crate) mod lexed;
-pub(crate) mod lexer;
-pub(crate) mod profile;
-
-// pub(crate) use lexed::{Lexed, LexerReader};
-pub use lexer::Lexer;
-pub use profile::{Profile, Token, Tokens};
